a4c1c2085c7e49f2c11f15e567ddf314d92a3f79
xue99999 <1090374167@qq.com>
2018-07-20 23:57:18 +0800

    add forms.py

--- simpledu/forms.py
+++ simpledu/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import Length, Email, EqualTo, Required
+from simpledu.models import db, User
 
 class RegisterForm(FlaskForm):
     username = StringField('用户名', validators=[Required(), Length(3, 24)])
@@ -9,8 +10,28 @@ class RegisterForm(FlaskForm):
     repeat_password = PasswordField('重复密码', validators=[Required(), EqualTo('password')])
     submit = SubmitField('提交')
 
+    def create_user(self):
+        user = User()
+        user.username = self.username.data
+        user.email = self.email.data
+        user.password = self.password.data
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+
+
 class LoginForm(FlaskForm):
     email = StringField('邮箱', validators=[Required(), Email(message='请输入合法的email地址')])
     password = PasswordField('密码', validators=[Required(), Length(6, 24)])
     remember_me = BooleanField('记住我')
     submit = SubmitField('提交')
+
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已经存在')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已经存在')
--- simpledu/handlers/front.py
+++ simpledu/handlers/front.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, url_for
 from simpledu.models import Course
 from simpledu.forms import LoginForm, RegisterForm
 
@@ -19,4 +19,9 @@ def login():
 @front.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+
+    if form.validate_on_submit():
+        form.create_user()
+        flash('注册成功，请登录！', 'success')
+        return redirect(url_for('.login'))
     return render_template('register.html', form=form)
